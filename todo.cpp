9a8ecc088afab396df10340d52a92db5faf216e3
Savoy Schuler <savoy.schuler@mines.sdsmt.edu>
2016-11-13 18:54:49 -0700

    todo added.

---
+++ todo.cpp
@@ -0,0 +1,21 @@
+//TODO:
+
+Planet jerk (wrong rotation)
+Light orbit
+Light Text
+Saturn's ring (tex map and orientation)
+Smooth controls
+Readme for controls
+Online menu
+Modularity
+
+
+//Extra:
+
+Starry background
+
+
+//End Steps:
+
+Document
+Refactor
