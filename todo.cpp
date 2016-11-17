8063925980565a711ef0fb059e28bab320922a04
Savoy Schuler <savoy.schuler@mines.sdsmt.edu>
2016-11-16 23:04:14 -0700

    todo updated.

--- todo.cpp
+++ todo.cpp
@@ -2,11 +2,7 @@
 
  - Modularize/Refactor orbits.cpp
  - Clean shit out of orbits.cpp and globals.
- - Planet color.
 
-//Extra:
-
- - Panning may also be accomplished by clicking the left mouse button and dragging.
 
 //End Steps:
 
