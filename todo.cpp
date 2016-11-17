b304514ca6f45d41d013e105a167bac9e88831d9
Savoy Schuler <savoy.schuler@mines.sdsmt.edu>
2016-11-16 18:10:50 -0700

    Todo updated.

--- todo.cpp
+++ todo.cpp
@@ -1,12 +1,7 @@
 //TODO:
 
- - Saturn's ring (tex map)
- - Sun 1/10th its size
- - Planetary orbits use the square root of the distance from the Sun.
- - Jupiter and Saturn are displayed at half their actual size.
  - Play with Starting position
  - Reset to default
- - fix look around
  - Inc/Dec Resolution
 
 
